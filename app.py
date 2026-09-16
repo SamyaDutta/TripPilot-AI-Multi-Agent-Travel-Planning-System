56fcf01c742c0e4a5c3a9634335b8a19b3687737
SamyaDutta <samyadutta29@gmail.com>
2026-09-17 03:29:44 +0530

    more changes

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ async def favicon():
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", "8000")),
         reload=True,
     )
